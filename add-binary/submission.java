69eee57e0339db7a39e453bbf3cffb5087e69e71
shreyas-thirumale <shreyas.thirumale25@gmail.com>
2026-07-02 17:27:35 -0400

    add add-binary

---
+++ add-binary/submission.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        StringBuilder ret = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            if (a.charAt(i) == 1 && b.charAt(j)) {
+
+            }
+        }
+    }
+}
